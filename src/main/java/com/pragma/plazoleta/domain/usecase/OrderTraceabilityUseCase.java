6c23cec4b4291e918f8e543fae8bf7efb884865b
jenner-durand-pragma <jenner.durand@pragma.com.co>
2026-06-21 16:14:26 -0500

    feat(order): implement save and find order states in use case

--- src/main/java/com/pragma/plazoleta/domain/usecase/OrderTraceabilityUseCase.java
+++ src/main/java/com/pragma/plazoleta/domain/usecase/OrderTraceabilityUseCase.java
@@ -1,11 +1,14 @@
 package com.pragma.plazoleta.domain.usecase;
 
 import com.pragma.plazoleta.domain.api.IOrderTraceabilityServicePort;
+import com.pragma.plazoleta.domain.exception.order.OrderNotBelongsToClientException;
+import com.pragma.plazoleta.domain.exception.order.OrderTraceabilityNotFoundException;
 import com.pragma.plazoleta.domain.model.OrderState;
 import com.pragma.plazoleta.domain.spi.IOrderTraceabilityPersistencePort;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 public class OrderTraceabilityUseCase implements IOrderTraceabilityServicePort {
@@ -14,11 +17,30 @@ public class OrderTraceabilityUseCase implements IOrderTraceabilityServicePort {
 
     @Override
     public void saveState(OrderState orderState) {
-        return;
+        orderTraceabilityPersistencePort.save(orderState);
     }
 
     @Override
     public List<OrderState> findByOrderIdForClient(Long orderId, Long clientId) {
-        return null;
+        var orderStates = orderTraceabilityPersistencePort.findByOrderId(orderId);
+
+        checkStatesNotEmpty(orderStates, orderId);
+        checkStatesBelongsToClient(orderStates, clientId);
+
+        return orderStates;
+    }
+
+    private void checkStatesNotEmpty(List<OrderState> orderStates, Long orderId) {
+        if (orderStates.isEmpty()) {
+            throw new OrderTraceabilityNotFoundException(orderId);
+        }
+    }
+
+    private void checkStatesBelongsToClient(List<OrderState> orderStates, Long clientId) {
+        var firstOrderState = orderStates.get(0);
+        if (!Objects.equals(firstOrderState.getClient().getId(), clientId)) {
+
+            throw new OrderNotBelongsToClientException(firstOrderState.getOrderId());
+        }
     }
 }
